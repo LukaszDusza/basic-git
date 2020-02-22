0e1333995e164dcefc4b72b59460fbd8f1ab2ae2
Lukasz Dusza <lukaszdusza280@gmail.com>
2020-02-22 12:19:27 +0100

    refaktor kodu z galezi master

--- src/akademia/Main.java
+++ src/akademia/Main.java
@@ -3,24 +3,6 @@ package akademia;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("kod główny bez brancha");
-
-        System.out.println("siema siema to jest brach person");
-
-        System.out.println("siema jest branch ficzer");
-
-        System.out.println("to jest super kod z ficzera 2");
-
-        System.out.println("to jest super kod z ficzera 3");
-
-        System.out.println("to jest super kod z ficzera 4");
-
-        System.out.println("tu zaczyna się wersja 1.0.1");
-
-        System.out.println("tutaj leży super kod z wersji 1.0.1");
-
-        System.out.println("tutaj jest koniec wersji 1.0.1 poszlo na produkcje");
-
-
+        System.out.println("kod ficzera 5");
     }
 }
