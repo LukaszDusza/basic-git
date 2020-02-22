3575f15e3e10cbb8683ce041bdb5932778483b7e
Lukasz Dusza <lukaszdusza280@gmail.com>
2020-02-22 10:06:12 +0100

    first commit in branch ficzer

--- src/akademia/Main.java
+++ src/akademia/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("kod główny bez brancha");
+
+        System.out.println("siema jest branch ficzer");
     }
 }
