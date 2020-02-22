cd5e25a066359034691e9b07a95b43d0c86eb59a
Lukasz Dusza <lukaszdusza280@gmail.com>
2020-02-22 12:34:28 +0100

    koniec refaktora

--- src/akademia/Main.java
+++ src/akademia/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("kod ficzera 5");
+        System.out.println("dalsza nieskonczona czesc refaktoru");
+        System.out.println("uff... koniec refaktoru z ficzera 5");
     }
 }
